78e90805c80c4eb31aaba47ea7d2eca96c3e6878
mttvns <mttvns@gmail.com>
2016-03-27 20:57:10 -0700

    change to comments to extract_ssx_zip_files.py

--- extract_ssx_zip_files.py
+++ extract_ssx_zip_files.py
@@ -9,6 +9,12 @@ ABOUT: This script extracts all zip files in a directory to a new folder in that
 same directory named the same as the zip file. SSX refers to the original file
 extension that was compressed into a zip file so that its contents could
 then be extracted. The original ssx files were saved as [filename].ssx.zip.
+
+.ssx files are proprietary files created by a company that manufactures
+CPR manikins that collect data about the CPR session for the purpose of
+training. The data is downloadable from the manikin in the form of a .ssx file.
+The files can be opened by converting them to zip files, which can then
+be extracted.
 '''
 
 print( 'Enter the path to the folder with the zip files to be extracted: ')
